0312ec45ca6d8b92c85db2e965afaf43c85cfe76
jwsdushscca <scott.hsu@hgiga.com>
2021-10-07 18:25:06 +0800

    first commit

---
+++ Highcharts/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using Highcharts.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Highcharts.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var result =new List<string>();
+            var iNitialHighchart = SetInitialHighcharts();
+            var title = new titleViewModel();
+            for (var i=0;i<10;i++)
+            {
+                title.text = $"第{i}題";
+                iNitialHighchart.title = title;
+                var series = new List<seriesViewModel>();
+                for(var j = 0; j < 5; j++)
+                {
+                    series.Add(GetSeries(j));
+                }
+                iNitialHighchart.series = series;
+                result.Add(JsonConvert.SerializeObject(iNitialHighchart));
+            }
+            return View(result);
+        }
+
+        private seriesViewModel GetSeries(int item)
+        {
+            var serie = new seriesViewModel();
+            serie.name = $"Series{item}";
+            List<int> data = new List<int>();
+            for (var i = 0; i < 5; i++)
+            {
+                data.Add(i* item);
+            }
+            serie.data = data;
+            return serie;
+        }
+
+        private HighchartsViewModel SetInitialHighcharts()
+        {
+            var xAxis = new xAxisViewModel()
+            {
+                categories = { "Pe", "Pd", "Pc", "Pb", "Pa" }
+            };
+
+            var yAxis = new yAxisViewModel()
+            {
+                max = 100,
+                min = 0,
+                title = { text = "人數百分比" },
+                plotLines = { (new plotLinesViewModel { value = 0, width = 0, color = "#808080" }) }
+
+            };
+            var tooltip = new tooltipViewModel()
+            { valueSuffix = "%" };
+            var legend = new legendViewModel()
+            {
+                layout = "vertical",
+                align = "right",
+                verticalAlign = "middle",
+                borderWidth = 0
+            };
+            var result = new HighchartsViewModel()
+            {
+                xAxis = xAxis,
+                yAxis = yAxis,
+                tooltip = tooltip,
+                legend = legend
+            };
+
+            return result;
+        }
+    }
+}
---
+++ Highcharts/Models/HighchartsViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Highcharts.Models
+{
+    public class HighchartsViewModel
+    {
+        public HighchartsViewModel()
+        {
+            title = new titleViewModel();
+            xAxis = new xAxisViewModel();
+            yAxis = new yAxisViewModel();
+            tooltip = new tooltipViewModel();
+            legend = new legendViewModel();
+            series = new List<seriesViewModel>();
+        }
+        public titleViewModel title { get; set; }
+        public xAxisViewModel xAxis { get; set; }
+        public yAxisViewModel yAxis { get; set; }
+        public tooltipViewModel tooltip { get; set; }
+        public legendViewModel legend { get; set; }
+        public List<seriesViewModel> series { get; set; }
+    }
+
+    public class seriesViewModel
+    {
+        public seriesViewModel()
+        {
+            data = new List<int>();
+        }
+        public string name { get; set; }
+        public List<int> data { get; set; }
+    }
+
+    public class xAxisViewModel
+    {
+        public xAxisViewModel()
+        {
+            categories = new List<string>();
+        }
+        public List<string> categories { get; set; }
+    }
+
+    public class yAxisViewModel
+    {
+        public yAxisViewModel()
+        {
+            title = new titleViewModel();
+            plotLines = new List<plotLinesViewModel>();
+
+        }
+        public int max { get; set; }
+        public int min { get; set; }
+        public titleViewModel title { get; set; }
+        public List<plotLinesViewModel> plotLines { get; set; }
+    }
+
+    public class titleViewModel
+    {
+        public string text { get; set; }
+    }
+
+    public class plotLinesViewModel
+    {
+        public int value { get; set; }
+        public int width { get; set; }
+        public string color { get; set; }
+
+    }
+
+    public class tooltipViewModel
+    {
+        public string valueSuffix { get; set; }
+    }
+
+    public class legendViewModel
+    {
+        public string layout { get; set; }
+
+        public string align { get; set; }
+
+        public string verticalAlign { get; set; }
+        public int borderWidth { get; set; }
+
+
+    }
+}
